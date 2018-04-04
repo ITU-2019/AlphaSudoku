3215af297903b0784701158faec780fed5ddbcce
Wrede <sbwr@itu.dk>
2018-04-04 12:49:51 +0200

    Add Project Files

---
+++ src/test/java/GradleTest.java
@@ -0,0 +1,12 @@
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class GradleTest{
+    
+    @Test
+    public void TestSimple() {
+        assertTrue(true);
+    }
+    
+}
