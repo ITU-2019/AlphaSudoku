828d3033e864f53970aa86ed5f7b7ac65991bf2e
MGApcDev <mgapcdev@gmail.com>
2018-04-18 15:57:40 +0200

    Add unit tests

--- src/main/java/SudokuSolver.java
+++ src/main/java/SudokuSolver.java
@@ -28,24 +28,24 @@ public class SudokuSolver implements ISudokuSolver {
 			D.add(i, block);
 		}
 
-		int[][] multi = new int[][]{
-		  { 8, 1, 2, 9, 7, 4, 3, 6, 5 },
-		  { 9, 3, 4, 6, 5, 1, 7, 8, 2 },
-		  { 7, 6, 5, 0, 2, 3, 9, 4, 1 },
-		  { 5, 7, 1, 0, 8, 2, 6, 9, 0 },
-		  { 2, 8, 9, 3, 6, 5, 4, 1, 0 },
-		  { 6, 4, 3, 7, 1, 9, 2, 5, 0 },
-		  { 1, 9, 6, 0, 3, 7, 8, 2, 4 },
-		  { 3, 2, 8, 0, 4, 6, 5, 7, 9 },
-		  { 4, 5, 7, 2, 9, 8, 1, 3, 6 }
-		};
-		readInPuzzle(multi);
+		// int[][] multi = new int[][]{
+		//   { 8, 1, 2, 9, 7, 4, 3, 6, 5 },
+		//   { 9, 3, 4, 6, 5, 1, 7, 8, 2 },
+		//   { 7, 6, 5, 0, 2, 3, 9, 4, 1 },
+		//   { 5, 7, 1, 0, 8, 2, 6, 9, 0 },
+		//   { 2, 8, 9, 3, 6, 5, 4, 1, 0 },
+		//   { 6, 4, 3, 7, 1, 9, 2, 5, 0 },
+		//   { 1, 9, 6, 0, 3, 7, 8, 2, 4 },
+		//   { 3, 2, 8, 0, 4, 6, 5, 7, 9 },
+		//   { 4, 5, 7, 2, 9, 8, 1, 3, 6 }
+		// };
+		// readInPuzzle(multi);
 	}
 
 
 	public boolean solve() {
 
-		ArrayList<Integer> asn = GetAssignment(puzzle);		
+		ArrayList<Integer> asn = GetAssignment(puzzle);
 		//if(!INITIAL_FC(asn)) return false;
 		if(!INITIAL_FC(asn)) return false;
 		return FC(asn) != null;
